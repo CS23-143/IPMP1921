2a4e5ea30ed5bb5f492591647b1fd0e12cea94f4
CS23-143 <81908222+CS23-143@users.noreply.github.com>
2021-04-07 22:46:07 +0530

    Create first unique character.cpp

---
+++ WEEK 2/strings_pdf/first unique character.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> a(s.length());
+        for(int i=0;i<s.length();i++){
+            if(a[i]==0){
+                int c=0;
+                for(int j=i+1;j<s.length();j++){
+                    if(s[i]==s[j]){
+                        c++;
+                        a[j]=1;
+                    }}
+                    if(c==0)
+                        return i;
+                
+            }
+        }
+        return -1;
+    }
+};
