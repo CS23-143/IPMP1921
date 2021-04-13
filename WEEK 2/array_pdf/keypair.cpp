5275ecf323058965dfd14b153fca44aa87eb1e50
CS23-143 <81908222+CS23-143@users.noreply.github.com>
2021-04-13 23:34:45 +0530

    Create keypair.cpp

---
+++ WEEK 2/array_pdf/keypair.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    for(int i=0;i<n;i++){
+	        for(int j=i+1;j<n;j++){
+	            if(arr[i]+arr[j]==x)
+	            return 1;
+	        }
+	    }
+	    return 0;
+	}
+};
