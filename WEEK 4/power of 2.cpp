38dcee87b2cfd93414d81647c2ea7b00f01f5b87
CS23-143 <81908222+CS23-143@users.noreply.github.com>
2021-04-23 21:42:13 +0530

    Update power of 2.cpp

--- WEEK 4/power of 2.cpp
+++ WEEK 4/power of 2.cpp
@@ -16,3 +16,13 @@ public:
                 return false;
     }
 };
+
+//or
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0) return false;
+        return ceil(log2(n))==floor(log2(n))?true : false;
+    }
+};
+
