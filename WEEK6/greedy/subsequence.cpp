57b95cf89270665cb0f96549969ab5f36a7416a9
CS23-143 <81908222+CS23-143@users.noreply.github.com>
2021-05-27 12:25:55 +0530

    Create subsequence.cpp

---
+++ WEEK6/greedy/subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0;
+        for(int i=0;i<t.size();i++){
+            if(t[i]==s[j]){
+                if(j==s.size())
+                    return true;
+                else
+                j++;
+            }
+            
+        }
+        if(j==s.size())
+            return true;
+            else
+        return false;
+    }
+};
