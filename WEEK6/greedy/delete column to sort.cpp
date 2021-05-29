75d67260cbeaef35c3feff8f4dca7dcb7ddcc0cc
CS23-143 <81908222+CS23-143@users.noreply.github.com>
2021-05-29 11:52:38 +0530

    Create delete column to sort.cpp

---
+++ WEEK6/greedy/delete column to sort.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int c=0;
+        if(strs.size()<=1)
+            return 0;
+        for(int i=0;i<strs[0].length();i++){
+            for(int j=1;j<strs.size();j++){
+                if(strs[j][i]<strs[j-1][i])
+                {c++;break;}
+            }
+        }
+        return c;
+    }
+};
