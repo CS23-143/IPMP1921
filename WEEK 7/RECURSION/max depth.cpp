5439495676646c2d9b9d61dcf77166b74e0013bd
CS23-143 <81908222+CS23-143@users.noreply.github.com>
2021-06-03 14:53:23 +0530

    https://leetcode.com/problems/maximum-depth-of-binary-tree/

--- WEEK 7/RECURSION/max depth.cpp
+++ WEEK 7/RECURSION/max depth.cpp
@@ -3,12 +3,6 @@ public:
     int maxDepth(TreeNode* root) {
         if(root==nullptr)
             return 0;
-        if(root->left==nullptr&&root->right==nullptr)
-            return 1;
-        else if(root->left==nullptr)
-            return 1+maxDepth(root->right);
-        else if(root->right==nullptr)
-            return 1+maxDepth(root->left);
         else
             return max(1+maxDepth(root->left),1+maxDepth(root->right));
     }
