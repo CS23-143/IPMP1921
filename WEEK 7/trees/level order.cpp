cdbb81b66dd399f89b071ce9b1d5f91d996d981f
CS23-143 <81908222+CS23-143@users.noreply.github.com>
2021-06-05 13:59:11 +0530

    https://leetcode.com/problems/binary-tree-level-order-traversal/

---
+++ WEEK 7/trees/level order.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> a;
+        vector<vector<int>> ans;
+       if(root==nullptr)
+    return ans;
+           a.push(root);
+        while(!a.empty()){
+            int j=a.size();
+            vector<int> p;
+            for(int i=0;i<j;i++){
+                TreeNode* te=a.front();
+                a.pop();
+                p.push_back(te->val);
+                
+                if(te->left)
+                a.push(te->left);
+                if(te->right)
+                a.push(te->right);
+            
+            }
+            ans.push_back(p);
+            p.clear();
+        }
+        return ans;
+    }
+};
